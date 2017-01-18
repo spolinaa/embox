0221d84d3748ebb8a359eb79b69136a702806af3
Alex Kalmuk <alexkalmuk@gmail.com>
2017-01-18 18:01:45 +0300

    stm32: Robot test now uses two motors

--- platform/stm32f4_multibots/cmds/main.c
+++ platform/stm32f4_multibots/cmds/main.c
@@ -32,16 +32,23 @@ static void init_leds() {
 }
 
 static void main_loop(void) {
-	struct stepper_motor motor;
+	struct stepper_motor motor1, motor2;
+
+	__GPIOA_CLK_ENABLE(); /* for motors */
+	__GPIOE_CLK_ENABLE(); /* for motors */
 
 	nrf24_enable();
-	motor_init(&motor);
-	motor_set_speed(&motor, MOTOR_MAX_SPEED);
+	motor_init(&motor1, GPIO_PIN_0, GPIO_PIN_2, GPIO_PIN_4, GPIO_PIN_6, GPIOA);
+	motor_init(&motor2, GPIO_PIN_8, GPIO_PIN_10, GPIO_PIN_12, GPIO_PIN_14, GPIOE);
+	motor_set_speed(&motor1, MOTOR_MAX_SPEED);
+	motor_set_speed(&motor2, MOTOR_MAX_SPEED);
 	ir_init();
 	ir_enable();
 
+	stm32_delay(1000000);
 	while (1) {
-		motor_do_steps(&motor, 3 * MOTOR_STEPS_PER_REVOLUTION, MOTOR_RUN_FORWARD);
+		motor_do_steps(&motor1, 3 * MOTOR_STEPS_PER_REVOLUTION, MOTOR_RUN_FORWARD);
+		motor_do_steps(&motor2, 3 * MOTOR_STEPS_PER_REVOLUTION, MOTOR_RUN_FORWARD);
 		stm32_delay(5000000);
 	}
 }
