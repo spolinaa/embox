cc266769978c7d39cde1d347aeb28e917187211f
Anton Bondarev <anton.bondarev2310@gmail.com>
2017-04-01 15:20:58 +0300

    drivers: Start work on dw_hdmi driver

---
+++ src/drivers/gpu/drm/dw_hdmi/dw_hdmi.c
@@ -0,0 +1,18 @@
+/**
+ * @file
+ *
+ * @date 30.03.2017
+ * @author Anton Bondarev
+ */
+
+#include <util/log.h>
+
+#include <embox/unit.h>
+
+#include "dw_hdmi.h"
+
+EMBOX_UNIT_INIT(dw_hdmi_init);
+
+static int dw_hdmi_init(void) {
+	return 0;
+}
