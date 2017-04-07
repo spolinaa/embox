e61c84f4d40003dff62f1a626abf6b608278b6ba
Alex Kalmuk <alexkalmuk@gmail.com>
2017-04-07 19:45:28 +0300

    robots: Improve the main robot test

--- platform/stm32f4_multibots/cmds/main.c
+++ platform/stm32f4_multibots/cmds/main.c
@@ -11,13 +11,20 @@
 #include <stdio.h>
 #include <assert.h>
 
+#include <util/log.h>
 #include <embox/unit.h>
+#include <kernel/thread.h>
+#include <kernel/time/ktime.h>
 
 #include "stm32f4_discovery.h"
 #include "stepper_motor.h"
 #include "ir.h"
 
+#define BH1750_CONTINUOUS_HIGH_RES_MODE  0x10
+
 extern void nrf24_enable(void);
+extern uint16_t gy_30_read_light_level(void);
+extern int gy_30_enable(void);
 
 static void stm32_delay(uint32_t delay) {
 	while (--delay > 0)
@@ -31,8 +38,24 @@ static void init_leds() {
 	BSP_LED_Init(LED6);
 }
 
+static void *light_sensor_loop(void *arg) {
+	uint16_t level;
+	int i;
+
+	while (1) {
+		for (i = 0; i < 10; i++) {
+			level = gy_30_read_light_level();
+			printf("light (lx): %d\n", level);
+			stm32_delay(1000);
+		}
+		ksleep(10000);
+	}
+	return NULL;
+}
+
 static void main_loop(void) {
 	struct stepper_motor motor1, motor2;
+	struct thread *th;
 
 	__GPIOD_CLK_ENABLE(); /* for motors */
 	__GPIOE_CLK_ENABLE(); /* for motors */
@@ -44,18 +67,30 @@ static void main_loop(void) {
 	motor_set_speed(&motor2, MOTOR_MAX_SPEED);
 	ir_init();
 	ir_enable();
+	gy_30_enable();
 
+	th = thread_create(THREAD_FLAG_SUSPENDED, light_sensor_loop, NULL);
+	if (!th) {
+		log_error("Failed to create thread\n");
+		return;
+	}
+	if (thread_launch(th) != 0) {
+		log_error("Failed to launch thread\n");
+		return;
+	}
+	if (thread_detach(th) != 0) {
+		log_error("Failed to detach thread\n");
+		return;
+	}
 	stm32_delay(1000000);
 	while (1) {
-		motor_do_steps2(&motor1, &motor2, 3 * MOTOR_STEPS_PER_REVOLUTION, MOTOR_RUN_FORWARD);
+		motor_do_steps2(&motor1, &motor2, 2 * MOTOR_STEPS_PER_REVOLUTION, MOTOR_RUN_BACKWARD);
 		stm32_delay(5000000);
 	}
 }
 
 int main(int argc, char *argv[]) {
 	printf("Robot test start!\n");
-
-	//HAL_Init();
 
 	init_leds();
 	BSP_LED_Toggle(LED6);
